f2ccdec4b5c72742c9c5a94d9cf3ac99d7ac00e6
mao <1296193245@qq.com>
2022-05-28 21:52:09 +0800

    elasticsearch_Implement_search

--- src/test/java/mao/elasticsearch_implement_search/ElasticSearchTest.java
+++ src/test/java/mao/elasticsearch_implement_search/ElasticSearchTest.java
@@ -1900,4 +1900,121 @@ public class ElasticSearchTest
             System.out.println("----------------------------------");
         }
     }
+
+
+    /**
+     * range query ,异步请求
+     * 范围查询
+     * <p>
+     * 请求内容：
+     * <pre>
+     *
+     * GET /book/_search
+     * {
+     *     "query":
+     *     {
+     *       "range":
+     *       {
+     *         "price":
+     *         {
+     *           "gte": 69.2,
+     *           "lte": 80
+     *         }
+     *       }
+     *     }
+     * }
+     *
+     * </pre>
+     * <p>
+     * 结果：
+     * <pre>
+     *
+     * 总数量：1
+     * 最大分数：1.0
+     *
+     *
+     * id:3
+     * score:1.0
+     * 内容：
+     * ---- price：78.6
+     * ---- studymodel：201001
+     * ---- name：spring开发基础
+     * ---- description：spring 在java领域非常流行，java程序员都在用。
+     * ---- pic：group1/M00/00/00/wKhlQFs6RCeAY0pHAAJx5ZjNDEM428.jpg
+     * ---- timestamp：2019-08-24 19:21:35
+     * ---- tags：[spring, java]
+     * ----------------------------------
+     *
+     * </pre>
+     *
+     * @throws Exception Exception
+     */
+    @Test
+    void search_range_query_async() throws Exception
+    {
+        //构建搜索请求
+        SearchRequest searchRequest = new SearchRequest("book");
+        //构建请求体
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //查询
+        searchSourceBuilder.query(QueryBuilders.rangeQuery("price").gte(69.2).lte(80));
+        //放入请求中
+        searchRequest.source(searchSourceBuilder);
+        //发起异步请求
+        client.searchAsync(searchRequest, RequestOptions.DEFAULT, new ActionListener<SearchResponse>()
+        {
+            /**
+             * 成功的回调
+             *
+             * @param searchResponse SearchResponse
+             */
+            @Override
+            public void onResponse(SearchResponse searchResponse)
+            {
+                //获取数据
+                SearchHits hits = searchResponse.getHits();
+                //总数
+                long value = hits.getTotalHits().value;
+                System.out.println("总数量：" + value);
+                float maxScore = hits.getMaxScore();
+                System.out.println("最大分数：" + maxScore);
+                System.out.println();
+                SearchHit[] hitsHits = hits.getHits();
+                //遍历数据
+                for (SearchHit hitsHit : hitsHits)
+                {
+                    System.out.println();
+                    //获得id
+                    String id = hitsHit.getId();
+                    //获得得分
+                    float score = hitsHit.getScore();
+                    //获得内容
+                    Map<String, Object> sourceAsMap = hitsHit.getSourceAsMap();
+
+                    //打印内容
+                    System.out.println("id:" + id);
+                    System.out.println("score:" + score);
+                    System.out.println("内容：");
+                    for (String key : sourceAsMap.keySet())
+                    {
+                        System.out.println("---- " + key + "：" + sourceAsMap.get(key));
+                    }
+                    System.out.println("----------------------------------");
+                }
+            }
+
+            /**
+             * 失败的回调
+             *
+             * @param e Exception
+             */
+            @Override
+            public void onFailure(Exception e)
+            {
+                e.printStackTrace();
+            }
+        });
+        //休眠2秒
+        Thread.sleep(2000);
+    }
 }
