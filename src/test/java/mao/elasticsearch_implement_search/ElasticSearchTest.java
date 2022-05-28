81dc046250e93c5f788dd279424f5fbbe576a122
mao <1296193245@qq.com>
2022-05-28 21:56:54 +0800

    elasticsearch_Implement_search

--- src/test/java/mao/elasticsearch_implement_search/ElasticSearchTest.java
+++ src/test/java/mao/elasticsearch_implement_search/ElasticSearchTest.java
@@ -2017,4 +2017,82 @@ public class ElasticSearchTest
         //休眠2秒
         Thread.sleep(2000);
     }
+
+
+    /**
+     * term query
+     * 字段为keyword时，存储和搜索都不分词
+     * 请求内容：
+     * <pre>
+     *
+     * GET /book/_search
+     * {
+     *     "query":
+     *     {
+     *       "term":
+     *       {
+     *         "description":
+     *         {
+     *           "value": "java程序员"
+     *         }
+     *       }
+     *     }
+     * }
+     *
+     * </pre>
+     *
+     * 结果：
+     * <pre>
+     *
+     * 总数量：0
+     * 最大分数：NaN
+     *
+     * </pre>
+     *
+     * @throws Exception Exception
+     */
+    @Test
+    void search_term_query() throws Exception
+    {
+        //构建搜索请求
+        SearchRequest searchRequest = new SearchRequest("book");
+        //构建请求体
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //查询
+        searchSourceBuilder.query(QueryBuilders.termQuery("description", "java程序员"));
+        //放入请求中
+        searchRequest.source(searchSourceBuilder);
+        //发起请求
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        //获取数据
+        SearchHits hits = searchResponse.getHits();
+        //总数
+        long value = hits.getTotalHits().value;
+        System.out.println("总数量：" + value);
+        float maxScore = hits.getMaxScore();
+        System.out.println("最大分数：" + maxScore);
+        System.out.println();
+        SearchHit[] hitsHits = hits.getHits();
+        //遍历数据
+        for (SearchHit hitsHit : hitsHits)
+        {
+            System.out.println();
+            //获得id
+            String id = hitsHit.getId();
+            //获得得分
+            float score = hitsHit.getScore();
+            //获得内容
+            Map<String, Object> sourceAsMap = hitsHit.getSourceAsMap();
+
+            //打印内容
+            System.out.println("id:" + id);
+            System.out.println("score:" + score);
+            System.out.println("内容：");
+            for (String key : sourceAsMap.keySet())
+            {
+                System.out.println("---- " + key + "：" + sourceAsMap.get(key));
+            }
+            System.out.println("----------------------------------");
+        }
+    }
 }
