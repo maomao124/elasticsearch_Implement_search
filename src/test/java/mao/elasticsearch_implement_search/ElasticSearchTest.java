ded9a3564d82a9f3506ab34151e35706093c96d2
mao <1296193245@qq.com>
2022-05-28 20:59:18 +0800

    elasticsearch_Implement_search

--- src/test/java/mao/elasticsearch_implement_search/ElasticSearchTest.java
+++ src/test/java/mao/elasticsearch_implement_search/ElasticSearchTest.java
@@ -267,5 +267,266 @@ public class ElasticSearchTest
         Thread.sleep(2000);
     }
 
+    /**
+     * 查询某字段
+     * 请求内容：
+     *
+     * <pre>
+     *
+     * GET /book/_search
+     * {
+     *   "query":
+     *   {
+     *     "match":
+     *     {
+     *       "description": "java"
+     *     }
+     *   }
+     * }
+     *
+     * </pre>
+     * <p>
+     * 结果：
+     * <pre>
+     *
+     * 总数量：4
+     *
+     *
+     * id:3
+     * score:0.4745544
+     * 内容：
+     * ---- price：78.6
+     * ---- studymodel：201001
+     * ---- name：spring开发基础
+     * ---- description：spring 在java领域非常流行，java程序员都在用。
+     * ---- pic：group1/M00/00/00/wKhlQFs6RCeAY0pHAAJx5ZjNDEM428.jpg
+     * ---- timestamp：2019-08-24 19:21:35
+     * ---- tags：[spring, java]
+     * ----------------------------------
+     *
+     * id:2
+     * score:0.33773077
+     * 内容：
+     * ---- price：68.6
+     * ---- studymodel：201001
+     * ---- name：java编程思想
+     * ---- description：java语言是世界第一编程语言，在软件开发领域使用人数最多。
+     * ---- pic：group1/M00/00/00/wKhlQFs6RCeAY0pHAAJx5ZjNDEM428.jpg
+     * ---- timestamp：2019-08-25 19:11:35
+     * ---- tags：[java, dev]
+     * ----------------------------------
+     *
+     * id:5
+     * score:0.33773077
+     * 内容：
+     * ---- price：68.6
+     * ---- studymodel：201001
+     * ---- name：java编程思想
+     * ---- description：java语言是世界第一编程语言，在软件开发领域使用人数最多。
+     * ---- pic：group1/M00/00/00/wKhlQFs6RCeAY0pHAAJx5ZjNDEM428.jpg
+     * ---- timestamp：2022-5-25 19:11:35
+     * ---- tags：[bootstrap, dev]
+     * ----------------------------------
+     *
+     * id:6
+     * score:0.33773077
+     * 内容：
+     * ---- price：68.6
+     * ---- studymodel：201001
+     * ---- name：java编程思想
+     * ---- description：java语言是世界第一编程语言，在软件开发领域使用人数最多。
+     * ---- pic：group1/M00/00/00/wKhlQFs6RCeAY0pHAAJx5ZjNDEM428.jpg
+     * ---- timestamp：2022-5-25 19:11:35
+     * ---- tags：[bootstrap, dev]
+     * ----------------------------------
+     *
+     * </pre>
+     *
+     * @throws Exception Exception
+     */
+    @Test
+    void search() throws Exception
+    {
+        //构建搜索请求
+        SearchRequest searchRequest = new SearchRequest("book");
+        //构建请求体
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //查询某字段
+        searchSourceBuilder.query(QueryBuilders.matchQuery("description", "java"));
+        //放入请求中
+        searchRequest.source(searchSourceBuilder);
+        //发起请求
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        //获取数据
+        SearchHits hits = searchResponse.getHits();
+        //总数
+        long value = hits.getTotalHits().value;
+        System.out.println("总数量：" + value);
+        System.out.println();
+        SearchHit[] hitsHits = hits.getHits();
+        //遍历数据
+        for (SearchHit hitsHit : hitsHits)
+        {
+            System.out.println();
+            //获得id
+            String id = hitsHit.getId();
+            //获得得分
+            float score = hitsHit.getScore();
+            //获得内容
+            Map<String, Object> sourceAsMap = hitsHit.getSourceAsMap();
 
+            //打印内容
+            System.out.println("id:" + id);
+            System.out.println("score:" + score);
+            System.out.println("内容：");
+            for (String key : sourceAsMap.keySet())
+            {
+                System.out.println("---- " + key + "：" + sourceAsMap.get(key));
+            }
+            System.out.println("----------------------------------");
+        }
+    }
+
+    /**
+     * 查询某字段，异步请求
+     * 请求内容：
+     *
+     * <pre>
+     *
+     * GET /book/_search
+     * {
+     *   "query":
+     *   {
+     *     "match":
+     *     {
+     *       "description": "java"
+     *     }
+     *   }
+     * }
+     *
+     * </pre>
+     * <p>
+     * 结果：
+     * <pre>
+     *
+     * 总数量：4
+     *
+     *
+     * id:3
+     * score:0.4745544
+     * 内容：
+     * ---- price：78.6
+     * ---- studymodel：201001
+     * ---- name：spring开发基础
+     * ---- description：spring 在java领域非常流行，java程序员都在用。
+     * ---- pic：group1/M00/00/00/wKhlQFs6RCeAY0pHAAJx5ZjNDEM428.jpg
+     * ---- timestamp：2019-08-24 19:21:35
+     * ---- tags：[spring, java]
+     * ----------------------------------
+     *
+     * id:2
+     * score:0.33773077
+     * 内容：
+     * ---- price：68.6
+     * ---- studymodel：201001
+     * ---- name：java编程思想
+     * ---- description：java语言是世界第一编程语言，在软件开发领域使用人数最多。
+     * ---- pic：group1/M00/00/00/wKhlQFs6RCeAY0pHAAJx5ZjNDEM428.jpg
+     * ---- timestamp：2019-08-25 19:11:35
+     * ---- tags：[java, dev]
+     * ----------------------------------
+     *
+     * id:5
+     * score:0.33773077
+     * 内容：
+     * ---- price：68.6
+     * ---- studymodel：201001
+     * ---- name：java编程思想
+     * ---- description：java语言是世界第一编程语言，在软件开发领域使用人数最多。
+     * ---- pic：group1/M00/00/00/wKhlQFs6RCeAY0pHAAJx5ZjNDEM428.jpg
+     * ---- timestamp：2022-5-25 19:11:35
+     * ---- tags：[bootstrap, dev]
+     * ----------------------------------
+     *
+     * id:6
+     * score:0.33773077
+     * 内容：
+     * ---- price：68.6
+     * ---- studymodel：201001
+     * ---- name：java编程思想
+     * ---- description：java语言是世界第一编程语言，在软件开发领域使用人数最多。
+     * ---- pic：group1/M00/00/00/wKhlQFs6RCeAY0pHAAJx5ZjNDEM428.jpg
+     * ---- timestamp：2022-5-25 19:11:35
+     * ---- tags：[bootstrap, dev]
+     * ----------------------------------
+     *
+     * </pre>
+     *
+     * @throws Exception Exception
+     */
+    @Test
+    void search_async() throws Exception
+    {
+        //构建搜索请求
+        SearchRequest searchRequest = new SearchRequest("book");
+        //构建请求体
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //查询某字段
+        searchSourceBuilder.query(QueryBuilders.matchQuery("description", "java"));
+        //放入请求中
+        searchRequest.source(searchSourceBuilder);
+        //发起异步请求
+        client.searchAsync(searchRequest, RequestOptions.DEFAULT, new ActionListener<SearchResponse>()
+        {
+            /**
+             * 成功的回调
+             *
+             * @param searchResponse SearchResponse
+             */
+            @Override
+            public void onResponse(SearchResponse searchResponse)
+            {
+                //获取数据
+                SearchHits hits = searchResponse.getHits();
+                //总数
+                long value = hits.getTotalHits().value;
+                System.out.println("总数量：" + value);
+                System.out.println();
+                SearchHit[] hitsHits = hits.getHits();
+                //遍历数据
+                for (SearchHit hitsHit : hitsHits)
+                {
+                    System.out.println();
+                    //获得id
+                    String id = hitsHit.getId();
+                    //获得得分
+                    float score = hitsHit.getScore();
+                    //获得内容
+                    Map<String, Object> sourceAsMap = hitsHit.getSourceAsMap();
+
+                    //打印内容
+                    System.out.println("id:" + id);
+                    System.out.println("score:" + score);
+                    System.out.println("内容：");
+                    for (String key : sourceAsMap.keySet())
+                    {
+                        System.out.println("---- " + key + "：" + sourceAsMap.get(key));
+                    }
+                    System.out.println("----------------------------------");
+                }
+            }
+
+            /**
+             * 失败的回调
+             *
+             * @param e Exception
+             */
+            @Override
+            public void onFailure(Exception e)
+            {
+                e.printStackTrace();
+            }
+        });
+        Thread.sleep(2000);
+    }
 }
